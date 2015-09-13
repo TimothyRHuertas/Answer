3d4a8ced307a1c1cfd744c98cda4b7b97d7f3f5c
Timothy Huertas <thuertas@apple.com>
2015-09-13 10:23:25 -0700

    Woring

--- src/com/google/challenges/Answer.java
+++ src/com/google/challenges/Answer.java
@@ -1,34 +1,37 @@
 package com.google.challenges;
 
 import java.util.*;
-import java.util.Map.Entry;
 
 //full disclosure.  i needed an implementation of
 //interval search in java
 //i took what i needed from here
 //https://github.com/kevinjdolan/intervaltree
 public class Answer {
-    public static void main(String[] args){
-        int[][] meetings4 = {{0, 1}, {1, 2}, {2, 3}, {3, 5}, {4, 5}};
-        int[][] meetings1 =  {{0, 1000000}, {42, 43}, {0, 1000000}, {42, 43}};
-        System.out.println(answer(meetings4));
-        System.out.println(answer(meetings1));
-    }
-
-
     public static int answer(int[][] meetings) {
         // Your code goes here.
         List<Interval> intervalList = new ArrayList<Interval>();
 
-        for(int i=0; i < meetings.length; i++){
+        for (int i = 0; i < meetings.length; i++) {
             intervalList.add(new Interval(meetings[i][0], meetings[i][1]));
+        }
+
+
+        return maxMeetings(intervalList, 1);
+    }
+
+    private static int maxMeetings(List<Interval> intervalList, int count) {
+        IntervalNode head = new IntervalNode(intervalList);
+        for (Interval interval : intervalList) {
+            interval.setCollisions(head.query(interval));
         }
 
         Collections.sort(intervalList, new Comparator<Interval>() {
             public int compare(Interval i1, Interval i2) {
-                if (i1.size() == i2.size()) {
+                int s1 = i1.getCollisions().size();
+                int s2 = i2.getCollisions().size();
+                if (s1 == s2) {
                     return 0;
-                } else if (i1.size() > i2.size()) {
+                } else if (s1 > s2) {
                     return 1;
                 } else {
                     return -1;
@@ -36,26 +39,19 @@ public class Answer {
             }
         });
 
-        return maxMeetings(intervalList, 1);
-    }
-
-    private static int maxMeetings(List<Interval> intervalList, int count){
-        IntervalNode head = new IntervalNode(intervalList);
         Interval intervalWithLeastCollisions = intervalList.get(0);
-        List<Interval> collisions = head.query(intervalWithLeastCollisions);
-        intervalList.removeAll(collisions);
-
-        if(intervalList.isEmpty()){
+        intervalList.removeAll(intervalWithLeastCollisions.getCollisions());
+        intervalList.remove(intervalWithLeastCollisions);
+        if (intervalList.isEmpty()) {
             return count;
-        }
-        else {
+        } else {
             return maxMeetings(intervalList, ++count);
         }
     }
 }
 
 
- class IntervalNode{
+class IntervalNode {
     private SortedMap<Interval, List<Interval>> intervals;
     private Integer center;
     private IntervalNode leftNode;
@@ -68,7 +64,7 @@ public class Answer {
 
         SortedSet<Integer> endpoints = new TreeSet<Integer>();
 
-        for(Interval interval: intervalList) {
+        for (Interval interval : intervalList) {
             endpoints.add(interval.getStart());
             endpoints.add(interval.getEnd());
         }
@@ -79,14 +75,14 @@ public class Answer {
         List<Interval> left = new ArrayList<Interval>();
         List<Interval> right = new ArrayList<Interval>();
 
-        for(Interval interval : intervalList) {
-            if(interval.getEnd() < median)
+        for (Interval interval : intervalList) {
+            if (interval.getEnd() < median)
                 left.add(interval);
-            else if(interval.getStart() > median)
+            else if (interval.getStart() > median)
                 right.add(interval);
             else {
                 List<Interval> posting = intervals.get(interval);
-                if(posting == null) {
+                if (posting == null) {
                     posting = new ArrayList<Interval>();
                     intervals.put(interval, posting);
                 }
@@ -94,33 +90,33 @@ public class Answer {
             }
         }
 
-        if(left.size() > 0)
+        if (left.size() > 0)
             leftNode = new IntervalNode(left);
-        if(right.size() > 0)
+        if (right.size() > 0)
             rightNode = new IntervalNode(right);
     }
 
 
-
     /**
      * Perform an interval intersection query on the node
+     *
      * @param target the interval to intersect
-     * @return		   all intervals containing time
+     * @return all intervals containing time
      */
     public List<Interval> query(Interval target) {
         List<Interval> result = new ArrayList<Interval>();
 
-        for(Interval key : intervals.keySet()) {
-            if(key.intersects(target))
-                for(Interval interval : intervals.get(key))
+        for (Interval key : intervals.keySet()) {
+            if (key.intersects(target))
+                for (Interval interval : intervals.get(key))
                     result.add(interval);
-            else if(key.getStart() > target.getEnd())
+            else if (key.getStart() > target.getEnd())
                 break;
         }
 
-        if(target.getStart() < center && leftNode != null)
+        if (target.getStart() < center && leftNode != null)
             result.addAll(leftNode.query(target));
-        if(target.getEnd() > center && rightNode != null)
+        if (target.getEnd() > center && rightNode != null)
             result.addAll(rightNode.query(target));
         return result;
     }
@@ -129,8 +125,8 @@ public class Answer {
     private Integer getMedian(SortedSet<Integer> set) {
         int i = 0;
         int middle = set.size() / 2;
-        for(Integer point : set) {
-            if(i == middle)
+        for (Integer point : set) {
+            if (i == middle)
                 return point;
             i++;
         }
@@ -139,18 +135,16 @@ public class Answer {
 }
 
 
- class Interval implements Comparable<Interval>{
+class Interval implements Comparable<Interval> {
     private int start;
     private int end;
+    private List<Interval> collisions;
 
     public Interval(int start, int end) {
         this.start = start;
         this.end = end;
     }
 
-    public int size(){
-        return end - start;
-    }
     public int getStart() {
         return start;
     }
@@ -163,14 +157,22 @@ public class Answer {
         return other.getEnd() > start && other.getStart() < end;
     }
 
+    public List<Interval> getCollisions() {
+        return collisions;
+    }
+
+    public void setCollisions(List<Interval> collisions) {
+        this.collisions = collisions;
+    }
+
     public int compareTo(Interval other) {
-        if(start < other.getStart())
+        if (start < other.getStart())
             return -1;
-        else if(start > other.getStart())
+        else if (start > other.getStart())
             return 1;
-        else if(end < other.getEnd())
+        else if (end < other.getEnd())
             return -1;
-        else if(end > other.getEnd())
+        else if (end > other.getEnd())
             return 1;
         else
             return 0;
